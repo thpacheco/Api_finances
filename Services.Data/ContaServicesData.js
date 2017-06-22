e54865f29dbb4cd3c7b090a69ff7fe2c732673b9
Thiago Pacheco <thiagopacheco30@gmail.com>
2017-06-21 23:28:43 -0300

    Ajuste services conta

--- Services.Data/ContaServicesData.js
+++ Services.Data/ContaServicesData.js
@@ -1,67 +1,62 @@
 var mongoose = require('mongoose');
 var Conta = mongoose.model('Conta');
 
-//GET - Return all Contas in the DB
+//GET - Return all Receitas in the DB
 exports.findAllContas = function (req, res) {
-	Conta.find(function (err, Contas) {
-		if (err) res.send(500, err.message);
-
-		console.log('GET /Contas')
-		res.status(200).jsonp(Contas);
-	});
+    Conta.find(function (err, contas) {
+        if (err) res.send(500, err.message);
+        res.status(200).jsonp(contas);
+    });
 };
 
-
-//GET - Return a Contas with specified ID
+//GET - Return a Receitas with specified ID
 exports.findById = function (req, res) {
-	Conta.findById(req.params.id, function (err, Conta) {
-		if (err) return res.send(500, err.message);
-		res.status(200).jsonp(Conta);
-	});
+    Conta.findById(req.params.id, function (err, conta) {
+        if (err) return res.send(500, err.message);
+        res.status(200).jsonp(conta);
+    });
 };
 
 
-//POST - Insert a new Conta in the DB
+//POST - Insert a new Receita in the DB
 exports.addConta = function (req, res) {
 
-	var Conta = new Conta({
-		nome: req.body.nome,
-		valor: req.body.valor,
-		diaMelhorCompra: req.diaMelhorCompra,
-		melhorDiaPagamento:req.melhorDiaPagamento,
-		tipoConta: req.body.tipoConta,
-		dataPagamento: req.body.dataPagamento,
-		status: req.body.status
-	});
+    var conta = new Conta({
+        nome: req.body.nome,
+        valor: req.body.valor,
+        tipoConta: req.body.tipoConta,
+        dataPagamento: req.body.dataPagamento,
+        status: req.body.status
+    });
 
-	Conta.save(function (err, Conta) {
-		if (err) return res.send(500, err.message);
-		res.status(200).jsonp(Conta);
-	});
+    conta.save(function (err, conta) {
+        if (err) return res.send(500, err.message);
+        res.status(200).jsonp(conta);
+    });
 };
 
 //PUT - Update a register already exists
 exports.updateConta = function (req, res) {
-	Conta.findById(req.params.id, function (err, Conta) {
-		Conta.nome = req.nome,
-			Conta.valor = req.valor,
-			Conta.tipoConta = req.tipoConta,
-			Conta.dataPagamento = req.dataPagamento,
-			Conta.status = req.status
+    Receita.findById(req.params.id, function (err, conta) {
+            conta.nome = req.nome,
+            conta.valor = req.valor,
+            conta.tipoConta = req.tipoConta,
+            conta.dataPagamento = req.dataPagamento,
+            conta.status = req.status
 
-		Conta.save(function (err) {
-			if (err) return res.send(500, err.message);
-			res.status(200).jsonp(Conta);
-		});
-	});
+        Conta.save(function (err) {
+            if (err) return res.send(500, err.message);
+            res.status(200).jsonp(conta);
+        });
+    });
 };
 
-//DELETE - Delete a Conta with specified ID
+//DELETE - Delete a Receita with specified ID
 exports.deleteConta = function (req, res) {
-	Conta.findById(req.params.id, function (err, Conta) {
-		Conta.remove(function (err) {
-			if (err) return res.send(500, err.message);
-			res.status(200);
-		})
-	});
+    Conta.findById(req.params.id, function (err, conta) {
+        Conta.remove(function (err) {
+            if (err) return res.send(500, err.message);
+            res.status(200);
+        })
+    });
 };
